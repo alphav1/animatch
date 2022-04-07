137cc275f168273cc6231e7cf4617d8572fc9284
alphav1 <miki.domalewski@gmail.com>
2022-04-07 21:40:29 +0200

    poopy farty

--- pages/routing4.js
+++ pages/routing4.js
@@ -22,11 +22,11 @@ export default function toPage() {
             <div>Watching genres: {Object.keys(data["watching"]).map(entry => <text> {entry}, </text>)}</div>
             <div>Completed genres: {Object.keys(data["completed"]).map(entry => <text> {entry}, </text>)}</div>
             <div> Your shared anime scores: </div><button onClick={async (e) => setScoreList(await getScoreComp(User1, User2))}> Compare Scores </button> <div> </div>
+            <div> Scores: {ScoreList.map(data1 => <ResultsTile data={data1} you={User1} friend={User2} />)} </div>
             <div> Your shared favorites: </div><button onClick={async (e) => { setFavoritesArray(await compareFavorites(await getFavorites(User1), await getFavorites(User2))); console.log("updated: " + FavoritesArray); }}>
                 Favorites </button>
             {/* <h4> Scores: {ScoreList.map(data => <body> title: {data.name} {User1} : {data.user1} {User2} : {data.user2} difference: {data.difference}</body>)} </h4> */}
             <div> Favorites: {FavoritesArray.flatMap(fav => <text>{fav}, </text>)} </div>
-            <div> Scores: {ScoreList.map(data1 => <ResultsTile data={data1} you={User1} friend={User2} />)} </div>
         </div>
     )
 }
